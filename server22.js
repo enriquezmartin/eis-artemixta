b6ceea81226ec95d688e42ca71b8c29b344c3a3f
Martín Enríquez <martin.hernan.enriquez@gmail.com>
2024-07-10 17:24:11 -0300

    Update server22.js

--- server22.js
+++ server22.js
@@ -8,15 +8,16 @@ const PORT = process.env.PORT || 5000;
 // Configurar CORS para permitir solicitudes desde tu dominio
 
 // Middleware para agregar encabezados CORS
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://artemixta-7caea27de0d2.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-});
+});*/
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors({ origin: "https://artemixta-7caea27de0d2.herokuapp.com", credentials: true }))
 
 let courses = require('./courses.json');
 let professors = require('./prof.json');
